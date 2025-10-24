bd874f78d0783ca11cf3479f09b38d236608220b
Your Name <you@example.com>
2025-10-24 19:39:05 +0300

    added logo plus reload functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  appType: "spa", // Ensures React Router SPA fallback
   plugins: [react()],
-})
+  server: {
+    port: 5173,
+    open: true,
+  },
+  preview: {
+    port: 4173,
+    open: true,
+  },
+});
+
+
+
+
